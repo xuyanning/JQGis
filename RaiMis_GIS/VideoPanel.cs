674b72656b588b4ed35270488a7cb9a3ecfe2604
xuyanning <xyn@sdu.edu.cn>
2017-06-27 12:04:24 +0800

    添加项目文件

--- RaiMis_GIS/VideoPanel.cs
+++ RaiMis_GIS/VideoPanel.cs
@@ -9,66 +9,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Vlc.DotNet.Forms;
+//using Vlc.DotNet.Forms;
 
 namespace RailwayGIS
 {
     public partial class VideoPanel : UserControl
     {
-        private VlcControl myVlcControl;
-        public VideoPanel()
-        {
-            InitializeComponent();
-            this.myVlcControl = new Vlc.DotNet.Forms.VlcControl();
-            ((System.ComponentModel.ISupportInitialize)(this.myVlcControl)).BeginInit();
-            // 
-            // myVlcControl
-            // 
-            this.myVlcControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.myVlcControl.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.myVlcControl.Location = new System.Drawing.Point(12, 12);
-            this.myVlcControl.Name = "myVlcControl";
-            this.myVlcControl.Size = new System.Drawing.Size(564, 338);
-            this.myVlcControl.TabIndex = 0;
-            this.myVlcControl.Text = "vlcRincewindControl1";
-            myVlcControl.Dock = DockStyle.Fill;
-            this.myVlcControl.VlcLibDirectory = null;
-            this.myVlcControl.VlcLibDirectoryNeeded += new System.EventHandler<Vlc.DotNet.Forms.VlcLibDirectoryNeededEventArgs>(this.OnVlcControlNeedLibDirectory);
-            //this.myVlcControl.LengthChanged += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerLengthChangedEventArgs>(this.OnVlcMediaLengthChanged);
-            //this.myVlcControl.Paused += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerPausedEventArgs>(this.OnVlcPaused);
-            //this.myVlcControl.Playing += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerPlayingEventArgs>(this.OnVlcPlaying);
-            //this.myVlcControl.PositionChanged += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerPositionChangedEventArgs>(this.OnVlcPositionChanged);
-            //this.myVlcControl.Stopped += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerStoppedEventArgs>(this.OnVlcStopped);
-            this.Controls.Add(this.myVlcControl);
-            ((System.ComponentModel.ISupportInitialize)(this.myVlcControl)).EndInit();
-            myVlcControl.Play(new Uri("http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_surround-fix.avi"));
+        //private VlcControl myVlcControl;
+        //public VideoPanel()
+        //{
+        //    InitializeComponent();
+        //    this.myVlcControl = new Vlc.DotNet.Forms.VlcControl();
+        //    ((System.ComponentModel.ISupportInitialize)(this.myVlcControl)).BeginInit();
+        //    // 
+        //    // myVlcControl
+        //    // 
+        //    this.myVlcControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+        //    | System.Windows.Forms.AnchorStyles.Left)
+        //    | System.Windows.Forms.AnchorStyles.Right)));
+        //    this.myVlcControl.BackColor = System.Drawing.SystemColors.ButtonShadow;
+        //    this.myVlcControl.Location = new System.Drawing.Point(12, 12);
+        //    this.myVlcControl.Name = "myVlcControl";
+        //    this.myVlcControl.Size = new System.Drawing.Size(564, 338);
+        //    this.myVlcControl.TabIndex = 0;
+        //    this.myVlcControl.Text = "vlcRincewindControl1";
+        //    myVlcControl.Dock = DockStyle.Fill;
+        //    this.myVlcControl.VlcLibDirectory = null;
+        //    this.myVlcControl.VlcLibDirectoryNeeded += new System.EventHandler<Vlc.DotNet.Forms.VlcLibDirectoryNeededEventArgs>(this.OnVlcControlNeedLibDirectory);
+        //    //this.myVlcControl.LengthChanged += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerLengthChangedEventArgs>(this.OnVlcMediaLengthChanged);
+        //    //this.myVlcControl.Paused += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerPausedEventArgs>(this.OnVlcPaused);
+        //    //this.myVlcControl.Playing += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerPlayingEventArgs>(this.OnVlcPlaying);
+        //    //this.myVlcControl.PositionChanged += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerPositionChangedEventArgs>(this.OnVlcPositionChanged);
+        //    //this.myVlcControl.Stopped += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerStoppedEventArgs>(this.OnVlcStopped);
+        //    this.Controls.Add(this.myVlcControl);
+        //    ((System.ComponentModel.ISupportInitialize)(this.myVlcControl)).EndInit();
+        //    myVlcControl.Play(new Uri("http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_surround-fix.avi"));
 
-        }
+        //}
 
-        private void OnVlcControlNeedLibDirectory(object sender, VlcLibDirectoryNeededEventArgs e)
-        {
-            var currentAssembly = Assembly.GetEntryAssembly();
-            var currentDirectory = new FileInfo(currentAssembly.Location).DirectoryName;
-            if (currentDirectory == null)
-                return;
-            if (AssemblyName.GetAssemblyName(currentAssembly.Location).ProcessorArchitecture == ProcessorArchitecture.X86)
-                e.VlcLibDirectory = new DirectoryInfo(Path.Combine(currentDirectory, @"lib\x86\"));
-            else
-                e.VlcLibDirectory = new DirectoryInfo(Path.Combine(currentDirectory, @"lib\x64\"));
+        //private void OnVlcControlNeedLibDirectory(object sender, VlcLibDirectoryNeededEventArgs e)
+        //{
+        //    var currentAssembly = Assembly.GetEntryAssembly();
+        //    var currentDirectory = new FileInfo(currentAssembly.Location).DirectoryName;
+        //    if (currentDirectory == null)
+        //        return;
+        //    if (AssemblyName.GetAssemblyName(currentAssembly.Location).ProcessorArchitecture == ProcessorArchitecture.X86)
+        //        e.VlcLibDirectory = new DirectoryInfo(Path.Combine(currentDirectory, @"lib\x86\"));
+        //    else
+        //        e.VlcLibDirectory = new DirectoryInfo(Path.Combine(currentDirectory, @"lib\x64\"));
 
-            if (!e.VlcLibDirectory.Exists)
-            {
-                var folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
-                folderBrowserDialog.Description = "Select Vlc libraries folder.";
-                folderBrowserDialog.RootFolder = Environment.SpecialFolder.Desktop;
-                folderBrowserDialog.ShowNewFolderButton = true;
-                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
-                {
-                    e.VlcLibDirectory = new DirectoryInfo(folderBrowserDialog.SelectedPath);
-                }
-            }
-        }
+        //    if (!e.VlcLibDirectory.Exists)
+        //    {
+        //        var folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+        //        folderBrowserDialog.Description = "Select Vlc libraries folder.";
+        //        folderBrowserDialog.RootFolder = Environment.SpecialFolder.Desktop;
+        //        folderBrowserDialog.ShowNewFolderButton = true;
+        //        if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+        //        {
+        //            e.VlcLibDirectory = new DirectoryInfo(folderBrowserDialog.SelectedPath);
+        //        }
+        //    }
+        //}
     }
 }
